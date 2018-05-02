cb484d781490e026e1174121618474d326b34555
Dark Souls <darkiisouls@outlook.com>
2018-05-03 00:02:12 +0300

    Final

--- app/src/main/java/com/example/darki/movies/DetailActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.darki.movies;
-
-import android.app.Activity;
-
-public class DetailActivity extends Activity {
-}
--- app/src/main/java/com/example/darki/movies/SettingsActivity.java
+++
@@ -1,4 +0,0 @@
-package com.example.darki.movies;
-
-public class SettingsActivity {
-}
